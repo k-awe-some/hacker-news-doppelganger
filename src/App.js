450837091704ab725fae16278ccb2991160fc713
Kay N <kaydigital.me@gmail.com>
2019-08-09 19:12:11 -0400

    updated App.js to render NewsFeed to ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+
 import "./App.css";
+import NewsFeed from "./components/news-feed/news-feed.component";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NewsFeed type="topstories" />
+    </div>
+  );
 }
 
 export default App;

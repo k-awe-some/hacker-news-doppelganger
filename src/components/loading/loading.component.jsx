cbe719124a4902b9728fb13a3de62b7caf508171
Kay N <kaynguyen.dev@gmail.com>
2019-08-10 19:29:28 -0400

    created Loading component

---
+++ src/components/loading/loading.component.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class Loading extends React.Component {
+  state = {
+    content: this.props.text
+  };
+
+  componentDidMount() {
+    const { text, speed } = this.props;
+    this.interval = window.setInterval(() => {
+      this.state.content === text + "..."
+        ? this.setState({ content: text })
+        : this.setState(({ content }) => ({
+            content: content + "."
+          }));
+    }, speed);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+
+  render() {
+    return <p>{this.state.content}</p>;
+  }
+}
+
+Loading.propTypes = {
+  text: PropTypes.string.isRequired,
+  speed: PropTypes.number.isRequired
+};
+
+Loading.defaultProps = {
+  text: "Loading",
+  speed: 300
+};
+
+export default Loading;

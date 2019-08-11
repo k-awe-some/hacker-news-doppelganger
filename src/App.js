0ac9dd8b4dd15a49680d92aef164007103620bed
Kay N <kaynguyen.dev@gmail.com>
2019-08-10 20:19:23 -0400

    added 404 page with Switch component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import NewsFeed from "./components/news-feed/news-feed.component";
@@ -10,9 +10,21 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" render={() => <NewsFeed type="topstories" />} />
-        <Route path="/user" component={User} />
-        <Route path="/post" component={Post} />
+        <Switch>
+          <Route exact path="/" render={() => <NewsFeed type="topstories" />} />
+          <Route
+            exact
+            path="/new"
+            render={() => <NewsFeed type="newstories" />}
+          />
+          <Route path="/user" component={User} />
+          <Route path="/post" component={Post} />
+          <Route
+            render={() => (
+              <h1>This page does not exist. Please enter a valid URL.</h1>
+            )}
+          />
+        </Switch>
       </div>
     </Router>
   );

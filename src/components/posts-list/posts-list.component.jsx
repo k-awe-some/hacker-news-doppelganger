bf996afb02d9ba75abc18b49d7cd50faeb3671c2
Kay N <kaynguyen.dev@gmail.com>
2019-08-12 20:03:16 -0400

    retouched styling

--- src/components/posts-list/posts-list.component.jsx
+++ src/components/posts-list/posts-list.component.jsx
@@ -5,26 +5,29 @@ import { formatDate } from "../../utils/helpers";
 import PostTitle from "../post-title/post-title.component";
 import MetaInfo from "../meta-info/meta-info.component";
 
-const PostsList = ({ posts }) => (
-  <ul>
-    {posts.map(post => (
-      <li key={post.id}>
-        <PostTitle
-          url={post.url}
-          title={post.title}
-          id={post.id}
-          fontSize="small"
-        />
-        <MetaInfo
-          id={post.id}
-          by={post.by}
-          time={formatDate(post.time)}
-          descendants={post.descendants}
-        />
-      </li>
-    ))}
-  </ul>
-);
+const PostsList = ({ posts }) =>
+  posts.length === 0 ? (
+    <p>This user hasn't posted yet.</p>
+  ) : (
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>
+          <PostTitle
+            url={post.url}
+            title={post.title}
+            id={post.id}
+            fontSize="small"
+          />
+          <MetaInfo
+            id={post.id}
+            by={post.by}
+            time={formatDate(post.time)}
+            descendants={post.descendants}
+          />
+        </li>
+      ))}
+    </ul>
+  );
 
 PostsList.propTypes = {
   posts: PropTypes.array.isRequired

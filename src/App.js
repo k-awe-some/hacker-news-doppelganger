1c233a7ff0beef4434e8958de4cf1a6be6fc9a9a
Kay N <kaynguyen.dev@gmail.com>
2019-08-13 18:58:57 -0400

    code split with React.lazy() & React.Suspense

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.scss";
 import NavBar from "./components/nav-bar/nav-bar.component";
-import NewsFeed from "./components/news-feed/news-feed.component";
-import User from "./components/user/user.component";
-import Post from "./components/post/post.component";
+import Loading from "./components/loading/loading.component";
 import { ThemeProvider } from "./utils/contexts/theme";
+
+const NewsFeed = React.lazy(() =>
+  import("./components/news-feed/news-feed.component")
+);
+const User = React.lazy(() => import("./components/user/user.component"));
+const Post = React.lazy(() => import("./components/post/post.component"));
 
 class App extends React.Component {
   state = {
@@ -24,30 +28,34 @@ class App extends React.Component {
           <div className={this.state.theme}>
             <div className="App">
               <NavBar />
-              <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={() => <NewsFeed type="topstories" />}
-                />
-                <Route
-                  exact
-                  path="/new"
-                  render={() => <NewsFeed type="newstories" />}
-                />
-                <Route
-                  exact
-                  path="/best"
-                  render={() => <NewsFeed type="beststories" />}
-                />
-                <Route path="/user" component={User} />
-                <Route path="/post" component={Post} />
-                <Route
-                  render={() => (
-                    <h1>This page does not exist. Please enter a valid URL.</h1>
-                  )}
-                />
-              </Switch>
+              <React.Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route
+                    exact
+                    path="/"
+                    render={() => <NewsFeed type="topstories" />}
+                  />
+                  <Route
+                    exact
+                    path="/new"
+                    render={() => <NewsFeed type="newstories" />}
+                  />
+                  <Route
+                    exact
+                    path="/best"
+                    render={() => <NewsFeed type="beststories" />}
+                  />
+                  <Route path="/user" component={User} />
+                  <Route path="/post" component={Post} />
+                  <Route
+                    render={() => (
+                      <h1>
+                        This page does not exist. Please enter a valid URL.
+                      </h1>
+                    )}
+                  />
+                </Switch>
+              </React.Suspense>
             </div>
           </div>
         </ThemeProvider>
